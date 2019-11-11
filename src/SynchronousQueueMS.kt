ec68ea8a7bcafe16d14b3fd29aa6032aed8f2345
Nikita Koval <nikita.koval@jetbrains.com>
2019-11-11 07:32:13 +0300

    Update SynchronousQueueMS.kt

--- src/SynchronousQueueMS.kt
+++ src/SynchronousQueueMS.kt
@@ -1,39 +1,15 @@
-import java.util.concurrent.locks.ReentrantLock
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class SynchronousQueueMS<E> : SynchronousQueue<E> {
-    private val senders = ArrayList<Pair<Continuation<Unit>, E>>() // pair = continuation + element
-    private val receivers = ArrayList<Continuation<E>>()
-    private val lock = ReentrantLock()
+    // TODO head and tail pointers
 
     override suspend fun send(element: E) {
-        lock.lock()
-        if (receivers.isNotEmpty()) {
-            val r = receivers.removeAt(0)
-            r.resume(element)
-            lock.unlock()
-        } else {
-            suspendCoroutine<Unit> { cont ->
-                senders.add(cont to element)
-                lock.unlock()
-            }
-        }
+        TODO("Implement me!")
     }
 
     override suspend fun receive(): E {
-        lock.lock()
-        if (senders.isNotEmpty()) {
-            val (s, elem) = senders.removeAt(0)
-            lock.unlock()
-            s.resume(Unit)
-            return elem
-        } else {
-            return suspendCoroutine sc@ { cont ->
-                receivers.add(cont)
-                lock.unlock()
-            }
-        }
+        TODO("Implement me!")
     }
 }
