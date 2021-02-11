63ddfc7f10ddcf6dc2dc319438dd15a827bd957c
grifguitar <xlootin@niuitmo.ru>
2021-02-11 17:07:51 +0300

    first commit

--- src/main/java/msqueue/MSQueue.java
+++ src/main/java/msqueue/MSQueue.java
@@ -1,47 +1,69 @@
 package msqueue;
 
+import kotlinx.atomicfu.AtomicRef;
+
 public class MSQueue implements Queue {
-    private Node head;
-    private Node tail;
+    private static class Node {
+        final int x;
+        final AtomicRef<Node> next;
+
+        Node(int x, Node next) {
+            this.x = x;
+            this.next = new AtomicRef<>(next);
+        }
+    }
+
+    private final AtomicRef<Node> head;
+    private final AtomicRef<Node> tail;
 
     public MSQueue() {
-        Node dummy = new Node(0);
-        this.head = dummy;
-        this.tail = dummy;
+        Node dummy = new Node(0, null);
+        this.head = new AtomicRef<>(dummy);
+        this.tail = new AtomicRef<>(dummy);
     }
 
     @Override
     public void enqueue(int x) {
-        Node newTail = new Node(x);
-        tail.next = newTail;
-        tail = newTail;
+        Node newTail = new Node(x, null);
+        while (true) {
+            Node curTail = tail.getValue();
+            if (curTail.next.compareAndSet(null, newTail)) {
+                tail.compareAndSet(curTail, newTail);
+                return;
+            } else {
+                tail.compareAndSet(curTail, curTail.next.getValue());
+            }
+        }
     }
 
     @Override
     public int dequeue() {
-        Node curHead = head;
-        Node next = head.next;
-        if (curHead == tail)
-            return Integer.MIN_VALUE;
-        head = next;
-        return next.x;
+        while (true) {
+            Node curHead = head.getValue();
+            Node curNextHead = curHead.next.getValue();
+            if (curNextHead == null) {
+                return Integer.MIN_VALUE;
+            }
+
+            Node curTail = tail.getValue();
+            if (curHead == curTail) {
+                tail.compareAndSet(curTail, curTail.next.getValue());
+            }
+
+            if (head.compareAndSet(curHead, curNextHead)) {
+                return curNextHead.x;
+            }
+        }
     }
 
     @Override
     public int peek() {
-        Node curHead = head;
-        Node next = head.next;
-        if (curHead == tail)
+        Node curHead = head.getValue();
+        Node curNextHead = curHead.next.getValue();
+        if (curNextHead == null) {
             return Integer.MIN_VALUE;
-        return next.x;
-    }
-
-    private class Node {
-        final int x;
-        Node next;
-
-        Node(int x) {
-            this.x = x;
+        } else {
+            return curNextHead.x;
         }
     }
 }
